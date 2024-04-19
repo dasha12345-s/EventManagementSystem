3bb5475fa7f8e3f305dc1e4340e6b0aaa7b94930
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-04-19 15:15:26 -0400

    Working with Query Parameters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import AuthenticationPage from './pages/Authentication'
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
+      {
+        path: 'auth',
+        element: <AuthenticationPage/>
+      },
       {
         path: 'events',
         element: <EventsRootLayout />,
