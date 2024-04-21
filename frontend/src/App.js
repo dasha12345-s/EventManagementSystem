0257b504e4338e20b3e47fabf4a6dfa5d131ad0f
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-04-21 12:16:15 -0400

    Routh Protection Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, {action as authAction} from './pages/Authentication'
 import { action as logoutAction} from './pages/Logout'
-import { tokenLoader } from './util/auth'
+import { tokenLoader, checkAuthLoader } from './util/auth'
 
 const router = createBrowserRouter([
   {
@@ -54,6 +54,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader,
               },
             ],
           },
@@ -61,6 +62,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
       },
